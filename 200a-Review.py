71494947069776aaabeca3ccfd9133e0ce5b8061
ethanDe <90716310+ethanDe@users.noreply.github.com>
2022-10-19 12:14:42 -0700

    In progress

--- 200a-Review.py
+++ 200a-Review.py
@@ -5,8 +5,12 @@ def getIntegers(myList):
     # myList : expected list or tuple
     # iterate through myList and add all the integers to the new list
     integers = [myList]
+    for i in integers:
+        if i != int:
+            integers.pop(i) #NEEDS RESTRUCTURE
 
     return integers
+
 
 def getFactor(myList,factor):
     # myList : expected list or tuple
@@ -16,7 +20,7 @@ def getFactor(myList,factor):
     factors = []
     for i in myList:
         if i%factor == 0:
-            print(f"{myList[]} is a factor of {i}")
+            print(f"{myList()} is a factor of {i}")
             factors.append(i)
         else:
             print(f"{myList} is a factor of {i}")
@@ -37,7 +41,10 @@ def getIntersection(list1,list2):
     # return a list of numbers that is in both lists
     # the intersection of the 2 number sets
     common = []
-
+    for i in list2:
+        if i in list1:
+            common.append(i)
+    
     return common
 
 def getUnion(list1,list2):
@@ -45,9 +52,12 @@ def getUnion(list1,list2):
     # list 2: expected list or tuple
     # return a list of numbers that is in either of the lists
     # the union of the 2 number sets
-    union = []
-
-    return union   
+    union = list1
+    for i in list2:
+        if i not in union:
+            union.append(i)
+    
+    return union
 
 def getMerge(list1,list2):
     # list 1: expected list or tuple
@@ -55,9 +65,12 @@ def getMerge(list1,list2):
     # add the elements of list2 into list1
     # if the list2 element is in list1, add it at the position where it occurs in list1
     # if the list2 element is not in list1, add it to the end
-    merge = list.copy()
+    merge = list.copy(list1)
+    for i in list2:
+        if i not in list1:
+            merge.append(i)
 
-    return merge
+    return merge #POTENTIALLY RESTRUCTURE
 
 
 def main():
@@ -65,7 +78,9 @@ def main():
     easy2 = [-2,-4,-6,2,4,6,0.1]
     numbers1 = [3,5,8,12,11,19,10,7,2,15,25,34,16,32,50,60,100,-3,0.25]
     numbers2 = [3,7,11,15,19,23,27,31,35,39,44,50]
-
+    #print(getMerge(easy1, easy2))
+    #print(getUnion(numbers1, numbers2))
+    print(getIntegers(numbers1))
 
 
 if __name__ == "__main__":
